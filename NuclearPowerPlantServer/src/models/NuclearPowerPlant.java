b12ffbcde0f4ff1d0097e5c734cfa4555e80d8d2
breaze <simon.gaviriao@gmail.com>
2020-10-05 23:03:23 -0500

    Models created

--- NuclearPowerPlantServer/src/models/NuclearPowerPlant.java
+++ NuclearPowerPlantServer/src/models/NuclearPowerPlant.java
@@ -5,15 +5,72 @@
  */
 package models;
 
+import exceptions.DamagedReactorException;
+import exceptions.ReactorAlreadyOffException;
+import exceptions.ReactorAlreadyOnException;
+
 /**
  *
  * @author Breaze
  */
 public class NuclearPowerPlant {
-    private NuclearReactor reactors[];
+    private final NuclearReactor reactors[];
     
     public NuclearPowerPlant(){
         this.reactors = new NuclearReactor[3];
+    }
+    
+    public boolean turnOn(int reactor){
+        boolean res = false;
+        try
+        {
+            res = this.reactors[reactor].turnOn();
+        }catch(DamagedReactorException | ReactorAlreadyOnException e){
+            System.out.println(e.getMessage());
+        }
+        return res;
+        
+    }
+    
+    public boolean turnOff(int reactor){
+        boolean res = false;
+        try
+        {
+            res = this.reactors[reactor].turnOff();
+        }catch(DamagedReactorException | ReactorAlreadyOffException e){
+            System.out.println(e.getMessage());
+        }
+        return res;
+        
+    }
+    
+    public boolean chargeReactor(int reactor, int value){
+        boolean res = false;
+        try
+        {
+            this.reactors[reactor].chargeReactor(value);
+            res = true;
+        }catch(DamagedReactorException e){
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+    
+    public boolean dischargeReactor(int reactor, int value){
+        boolean res = false;
+        try
+        {
+            this.reactors[reactor].dischargeReactor(value);
+            res = true;
+        }catch(DamagedReactorException e){
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+    
+    public boolean repairReactor(int reactor){
+        boolean res = this.reactors[reactor].repairReactor();
+        return res;
     }
     
 }
--- NuclearPowerPlantServer/src/models/NuclearReactor.java
+++ NuclearPowerPlantServer/src/models/NuclearReactor.java
@@ -5,11 +5,73 @@
  */
 package models;
 
+import exceptions.DamagedReactorException;
+import exceptions.ReactorAlreadyOffException;
+import exceptions.ReactorAlreadyOnException;
+
 /**
  *
  * @author Breaze
  */
 public class NuclearReactor {
-    private int charge;
+    private float charge;
+    private boolean isOn;
+    private String status;
+    
+    public boolean turnOn(){
+        boolean res = false;
+        if(!isWorking())
+            throw new DamagedReactorException("The reactor is damaged and cannot be turned on");
+        if(isOn)
+           throw new ReactorAlreadyOnException("The reactor is already on");
+        this.isOn = true;
+        res = true;
+        return res;
+    }
+    
+    public boolean turnOff(){
+        boolean res = false;
+        if(!isWorking())
+            throw new DamagedReactorException("The reactor is damaged and cannot be turned off");
+        if(!isOn)
+           throw new ReactorAlreadyOffException("The reactor is already off");
+        this.isOn = false;
+        this.charge = 0;
+        res = true;
+        return res;
+    }
+    
+    public void chargeReactor(int value){
+        if(!isWorking())
+            throw new DamagedReactorException("The reactor is damaged and cannot be charged");
+        this.charge+=value;
+        if(this.charge>100)
+        {
+            this.status = "damaged";
+            throw new DamagedReactorException("The reactor is damaged due the charge level surpassed the limit");
+        }
+    }
+    
+    public void dischargeReactor(int value){
+        if(!isWorking())
+            throw new DamagedReactorException("The reactor is damaged and cannot be discharged");
+        this.charge-=value;
+        if(this.charge<0)
+            this.charge = 0;
+    }
+    
+    public boolean repairReactor(){
+        boolean res = false;
+        if(isWorking())
+            return res;
+        res = true;
+        this.status = "working";
+        return res;
+    }
+    
+    public boolean isWorking(){
+        boolean isWorking = (this.status.equals("working"));
+        return isWorking;
+    }
     
 }
